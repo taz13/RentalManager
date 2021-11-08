e6ef1f67b2527f64135d2a1a73cf5fd6033d5a1e
tahseenrahman <tahseen137@gmail.com>
2021-11-08 16:57:40 -0500

    hard coded call to zillow api

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Python application for Rental Management
 This a python web application built on Django framework. This application will have different features to help manage rental properties.
 
 **Environment Setup**
+
 Run "setup.sh" and it will install homebrew,python,pip and django.
--- rentEstimator/views.py
+++ rentEstimator/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+
+
+    url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
+
+    querystring = {"location": "Ontario", "status_type": "ForSale", "home_type": "Houses, Townhomes"}
+
+    headers = {
+        'x-rapidapi-host': "zillow-com1.p.rapidapi.com",
+        'x-rapidapi-key': "be7b441027msh62351d6edecc63ap1c97a2jsnc5e03c8e0fe0"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return HttpResponse(response.text)
