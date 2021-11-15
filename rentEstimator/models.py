dec8744afee477a653bf6a3922be1df745cf3ae5
tahseenrahman <tahseen137@gmail.com>
2021-11-15 08:22:51 -0500

    Fixed bug with the city name for Sudbury

--- rentEstimator/models.py
+++ rentEstimator/models.py
@@ -5,16 +5,22 @@ class PropertyInfo:
     def __init__(self, propertyObj):
         m = mortgage.Mortgage(interest=0.0175, amount=propertyObj['price'], months=360)
         mapUrlFormat= "https://www.google.ca/maps/@{lat:.5f},{long:.5f},16.33z"
+
+        city=propertyObj['address'].split(",")[1].strip();
+
+        if city == "Greater Sudbury / Grand Sudbury":
+            city="Sudbury"
         if propertyObj['latitude'] is None or propertyObj['longitude'] is None:
             mapUrl = ""
         else:
             mapUrl = mapUrlFormat.format(lat=propertyObj['latitude'], long=propertyObj['longitude'])
+
         self.propertyInfoDict= {'zpId': propertyObj['zpid'], 'address': propertyObj['address'],
                                 'country': propertyObj['country'], 'currency': propertyObj['currency'],
                                 'imgSrc': propertyObj['imgSrc'], 'latitude': propertyObj['latitude'],
                                 'longitude': propertyObj['longitude'], 'price': propertyObj['price'],
                                 'propertyType': propertyObj['propertyType'],
-                                'city': propertyObj['address'].split(",")[1].strip(), 'minRent': propertyObj['price'] * 0.0075,
+                                'city': city, 'minRent': propertyObj['price'] * 0.0075,
                                 'monthlyMortgage': m.monthly_payment(), 'mapUrl':mapUrl}
 
 
